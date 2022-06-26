9528465f9dcfaf0ef568f2e0217967d3615da634
neo0057 <neo0057@gmail.com>
2022-06-26 17:10:47 +0530

    adding status code

--- src/main/java/com/smaato/interview/server/service/AppService.java
+++ src/main/java/com/smaato/interview/server/service/AppService.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -33,6 +34,6 @@ public class AppService {
         } else {
             logger.info("endpoint is empty");
         }
-        return (isHttpPostFeatureEnabled && response != null) ? response.getStatusCodeValue() : 200;
+        return (isHttpPostFeatureEnabled && response != null) ? response.getStatusCodeValue() : HttpStatus.OK.value();
     }
 }
